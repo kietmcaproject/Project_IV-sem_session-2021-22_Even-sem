77b4838ff0579a601c1e2d7a88e8af9e2702e3e0
Piyush Kumar <82364451+piyush1114@users.noreply.github.com>
2022-05-31 21:13:42 +0530

    Add files via upload

---
+++ G43_A Trip To Remember/Assessment CT2/ATripToRemember.Utility/SD.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATripToRemember.Utility
+{
+    public static class SD
+    {
+        public const string Role_Admin = "Admin";
+        public const string Role_User = "User";
+
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusPaid = "Paid";
+        public const string PaymentStatusRejected = "Rejected";
+
+        public const string BookingStatusRequested = "Requested";
+        public const string BookingStatusApproved = "Approved";
+        public const string BookingStatusCompleted = "Completed";
+    }
+}
