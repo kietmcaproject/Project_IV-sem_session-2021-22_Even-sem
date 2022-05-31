feb2b4edb4719804c5a7dac8812e0552a2e29bcb
rishabhkiet <105513985+rishabhkiet@users.noreply.github.com>
2022-05-31 21:40:48 +0530

    Add files via upload

---
+++ G-3 Real time weather forecasting app/Source code/.eslintrc.js
@@ -0,0 +1,67 @@
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true
+		}
+	},
+	settings: {
+		react: {
+			version: 'detect'
+		},
+		'import/resolver': {
+			node: {
+				paths: ['src'],
+				extensions: ['.js', '.jsx', '.ts', '.tsx']
+			}
+		}
+	},
+	env: {
+		browser: true,
+		amd: true,
+		node: true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:jsx-a11y/recommended',
+		'plugin:prettier/recommended' // Make sure this is always the last element in the array.
+	],
+	plugins: ['simple-import-sort', '@typescript-eslint', 'unused-imports', 'prettier'],
+	rules: {
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': ['error'],
+		'prettier/prettier': ['warn', {}, { usePrettierrc: true }],
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
+		'react/prop-types': 'off',
+		'react/jsx-uses-react': 'off',
+		'react/react-in-jsx-scope': 'off',
+		'jsx-a11y/accessible-emoji': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'simple-import-sort/imports': 'error',
+		'simple-import-sort/exports': 'error',
+		'unused-imports/no-unused-imports': 'error',
+		'unused-imports/no-unused-vars': [
+			'warn',
+			{
+				vars: 'all',
+				varsIgnorePattern: '^_',
+				args: 'after-used',
+				argsIgnorePattern: '^_'
+			}
+		],
+		'jsx-a11y/anchor-is-valid': [
+			'error',
+			{
+				components: ['Link'],
+				specialLink: ['hrefLeft', 'hrefRight'],
+				aspects: ['invalidHref', 'preferButton']
+			}
+		]
+	}
+};
