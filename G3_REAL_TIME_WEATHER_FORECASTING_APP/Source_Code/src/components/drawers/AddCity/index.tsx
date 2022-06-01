8610b89e87b88917c6a78493c8a936f17ebec240
rishabhkiet <105513985+rishabhkiet@users.noreply.github.com>
2022-06-01 09:38:40 +0530

    Add files via upload

---
+++ G3_REAL_TIME_WEATHER_FORECASTING_APP/Source_Code/src/components/drawers/AddCity/index.tsx
@@ -0,0 +1,184 @@
+import {
+	Button,
+	Drawer,
+	DrawerBody,
+	DrawerContent,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerOverlay,
+	DrawerProps,
+	Flex,
+	FormControl,
+	FormLabel,
+	Heading,
+	Icon,
+	IconButton,
+	Select,
+	useColorModeValue,
+	useToast
+} from '@chakra-ui/react';
+import { CountriesContext } from 'contexts/CountriesContext';
+import { WeatherContext } from 'contexts/WeatherContext';
+import { ChangeEvent, FormEvent, useContext, useState } from 'react';
+import { IoClose, IoSave } from 'react-icons/io5';
+import { SavedCityType } from 'shared/@types/WeatherContext';
+
+type Props = Omit<DrawerProps, 'children'>;
+
+const AddCityDrawer = ({ isOpen, onClose, ...props }: Props) => {
+	const { countries } = useContext(CountriesContext);
+	const { addCity } = useContext(WeatherContext);
+	const toast = useToast();
+
+	const [data, setData] = useState<Omit<SavedCityType, 'coordinates'>>({
+		country: '',
+		name: ''
+	});
+
+	const handleInputChange = (evt: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+		const { name, value } = evt.target;
+
+		// Avoid problems with second select when change the country
+		if (name === 'country' && data.name !== '') {
+			setData({
+				name: '',
+				[name]: value
+			});
+			return;
+		}
+
+		setData((prevState) => ({
+			...prevState,
+			[name]: value
+		}));
+	};
+
+	// Handle submit the values
+	const handleSubmit = (evt: FormEvent) => {
+		evt.preventDefault();
+
+		if (!data.country || !data.name) {
+			toast({
+				description: `Please fill the city and the country field`,
+				status: 'error',
+				isClosable: true
+			});
+			return;
+		}
+
+		try {
+			addCity(data);
+
+			toast({
+				description: `City ${data.name} added successfully`,
+				status: 'success',
+				isClosable: true
+			});
+
+			onClose();
+
+			setData({
+				name: '',
+				country: ''
+			});
+		} catch (error) {
+			toast({
+				description: `Occurred an error while adding the city. Please try again.`,
+				status: 'error',
+				isClosable: true
+			});
+		}
+	};
+
+	return (
+		<Drawer isOpen={isOpen} placement={'right'} onClose={onClose} {...props} size={'md'}>
+			<DrawerOverlay />
+
+			<DrawerContent>
+				<DrawerHeader as={Flex} alignItems={'center'} justifyContent={'space-between'}>
+					<Heading as={'h3'} fontSize={'md'} fontWeight={'bold'}>
+						Add City
+					</Heading>
+					<IconButton
+						aria-label={'Close'}
+						onClick={onClose}
+						icon={
+							<Icon
+								as={IoClose}
+								color={useColorModeValue('primary.500', 'secondary.500')}
+							/>
+						}
+					/>
+				</DrawerHeader>
+				<DrawerBody>
+					<Flex
+						as={'form'}
+						onSubmit={handleSubmit}
+						id={'add-form'}
+						flexDir={'column'}
+						gap={4}
+					>
+						<FormControl>
+							<FormLabel htmlFor="country">Country</FormLabel>
+							<Select
+								onChange={handleInputChange}
+								name={'country'}
+								variant="filled"
+								placeholder="Select a country"
+							>
+								{countries.map((country) => (
+									<option key={country.iso2} value={country.iso2}>
+										{country.country}
+									</option>
+								))}
+							</Select>
+						</FormControl>
+
+						<FormControl>
+							<FormLabel htmlFor="city">City</FormLabel>
+							<Select
+								disabled={!data.country}
+								onChange={handleInputChange}
+								name={'name'}
+								variant="filled"
+								placeholder="Select a city"
+							>
+								{countries.filter((country) => country.iso2 === data.country)
+									.length > 0 &&
+									countries
+										.filter((country) => country.iso2 === data.country)[0]
+										.cities.map((city) => (
+											<option key={city} value={city}>
+												{city}
+											</option>
+										))}
+							</Select>
+						</FormControl>
+					</Flex>
+				</DrawerBody>
+
+				<DrawerFooter as={Flex} gap={2} alignItems={'center'} justifyContent={'flex-end'}>
+					<Button
+						type={'button'}
+						variant="outline"
+						onClick={onClose}
+						leftIcon={<Icon as={IoClose} />}
+					>
+						Cancel
+					</Button>
+					<Button
+						disabled={!data.country || !data.name}
+						type={'submit'}
+						form={'add-form'}
+						colorScheme={useColorModeValue('primary', 'secondary')}
+						leftIcon={<Icon as={IoSave} />}
+					>
+						Save
+					</Button>
+				</DrawerFooter>
+			</DrawerContent>
+		</Drawer>
+	);
+};
+
+export default AddCityDrawer;
---
+++ G3_REAL_TIME_WEATHER_FORECASTING_APP/Source_Code/src/pages/Home/index.tsx
@@ -0,0 +1,74 @@
+import {
+	Box,
+	Button,
+	Container,
+	Flex,
+	Heading,
+	Icon,
+	SimpleGrid,
+	useDisclosure
+} from '@chakra-ui/react';
+import sunnyBackground from 'assets/media/backgrounds/sunny.jpg';
+import WeatherCard from 'components/cards/Weather';
+import AddCityDrawer from 'components/drawers/AddCity';
+import { WeatherContext } from 'contexts/WeatherContext';
+import { useContext } from 'react';
+import { BsPlusLg } from 'react-icons/bs';
+
+const Home = () => {
+	const { cities } = useContext(WeatherContext);
+
+	// Chakra UI Drawer Handle (to Add City Drawer)
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
+	return (
+		<main>
+			<Box
+				paddingY={6}
+				backgroundImage={sunnyBackground}
+				backgroundPosition={'center'}
+				backgroundSize={'cover'}
+				backgroundColor={'blackAlpha.700'}
+				backgroundBlendMode={'overlay'}
+				as={Flex}
+				flexDir={'column'}
+				justifyContent={'center'}
+				height={'400px'}
+			>
+				<Container maxW={'container.lg'}>
+					<Heading as={'h1'} fontWeight={'bold'} fontSize={'4xl'} color={'white'}>
+						Tracked Cities
+					</Heading>
+					<Heading as={'h2'} fontWeight={400} fontSize={'2xl'} color={'white'}>
+						All the cities you are saved to see the weather!
+					</Heading>
+
+					<Button
+						onClick={onOpen}
+						leftIcon={<Icon as={BsPlusLg} fontSize={'14px'} />}
+						mt={10}
+					>
+						Add City
+					</Button>
+
+					<AddCityDrawer isOpen={isOpen} onClose={onClose} />
+				</Container>
+			</Box>
+			<Container as={'section'} maxW={'container.lg'} py={10}>
+				{cities.length > 0 ? (
+					<SimpleGrid columns={[1, 2, 3]} spacing={2}>
+						{cities.map((city) => (
+							<WeatherCard city={city} key={city.name} />
+						))}
+					</SimpleGrid>
+				) : (
+					<Heading as={'h3'} fontSize={'xl'} textAlign={'center'} fontWeight={'regular'}>
+						Please add at least one city to track!
+					</Heading>
+				)}
+			</Container>
+		</main>
+	);
+};
+
+export default Home;
