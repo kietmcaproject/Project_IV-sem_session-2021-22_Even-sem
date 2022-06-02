07ad3e45a570b33f4519b141de46180a9a552143
arsangeta <65891020+arsangeta@users.noreply.github.com>
2022-06-02 13:31:14 +0530

    Add files via upload

---
+++ G_43 A Trip To Remember/A Trip to Remember/wwwroot/js/airline.js
@@ -0,0 +1,62 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+})
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Airline/GetAll"
+        },
+        "columns": [
+            {
+                "data": "imageUrl",
+                "render": function (imageUrl) {
+                    return `
+                        <img src="${imageUrl}" style="width: 150px; height: 100px; border: 1px solid #bbb9b9" />
+                    `
+                },
+                "width": "15%"
+            },
+            { "data": "name", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="w-75 btn-group" role="group">
+                            <a href="/Admin/Airline/Upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i>Edit</a>
+                            <a onClick=Delete('/Admin/Airline/Delete/${data}') class="btn btn-danger mx-2"><i class="bi bi-trash-fill"></i>Delete</a>
+                        </div>
+                    `
+                },
+                "width": "15%"
+            }
+        ]
+    });
+}
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    if (data.success) {
+                        dataTable.ajax.reload();
+                        toastr.success(data.message);
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            })
+        }
+    })
+}
---
+++ G_43 A Trip To Remember/A Trip to Remember/wwwroot/js/flight.js
@@ -0,0 +1,77 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+})
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Flight/GetAll"
+        },
+        "columns": [
+            {
+                "data": "airline",
+                "render": function (airline) {
+                    return `
+                        <img src="${airline.imageUrl}" style="width: 90px; height: 60px; border: 1px solid #bbb9b9" />&nbsp;
+                        ${airline.name}
+                    `
+                },
+                "width": "20%"
+            },
+            { "data": "startingLocation", "width": "10%" },
+            { "data": "destination", "width": "10%" },
+            { "data": "startingTime", "width": "15%"},
+            { "data": "reachingTime", "width": "15%" },
+            {
+                "data": "price",
+                "render": function (price) {
+                    return price.toLocaleString('en-IN', {
+                        maximumFractionDigits: 2,
+                        style: 'currency',
+                        currency: 'INR'
+                    });
+                },
+                "width": "10%"
+            },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="w-75 btn-group" role="group">
+                            <a href="/Admin/Flight/Upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i>Edit</a>
+                            <a onClick=Delete('/Admin/Flight/Delete/${data}') class="btn btn-danger mx-2"><i class="bi bi-trash-fill"></i>Delete</a>
+                        </div>
+                    `
+                },
+                "width": "20%"
+            }
+        ]
+    });
+}
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    if (data.success) {
+                        dataTable.ajax.reload();
+                        toastr.success(data.message);
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            })
+        }
+    })
+}
